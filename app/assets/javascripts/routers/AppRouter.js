cbec9783983ccff465d8891e72e0bbd6b4b19192
Pinky <dir.michaellin@gmail.com>
2016-10-26 11:22:42 +1100

    Add backbone back end functionality

--- app/assets/javascripts/routers/AppRouter.js
+++ app/assets/javascripts/routers/AppRouter.js
@@ -5,11 +5,37 @@ app.Router = Backbone.Router.extend({
     "": "index",
     "airplanes": "airplane",
     "flights": "flight",
-    "flights/:id":"booking"
+    "flights/:id":"booking",
+    "search": "viewSearch"
   },
 
   index: function() {
-    // var appView = new app.AppView();
-    // appView.render
+    $('#main').hide();
+  },
+
+  // viewAirplane: function()
+
+  viewFlight: function(id) {
+    $("#main").show();
+    $("#main").empty();
+    app.burningAirplanes.fetch().done(function () {
+      //TODO make an app.burningFlights
+      var flight = app.burningFlights.get(id);
+      // TODO make an app for flight view.
+      //TODO Make model for flights.
+      var flightView = new app.FlightView({model: flight});
+      flightView.render();
+    });
+  },
+
+  viewSearch: function () {
+    $('#main').show();
+    $('#main').empty();
+    console.log('search');
+    // TODO Make a collection for app.Flights
+    var searchView = new app.SearchView({collection: app.Flights});
+    //TODO Make searchView
+    searchView.render()
   }
+
 });
--- app/assets/javascripts/views/AppView.js
+++ app/assets/javascripts/views/AppView.js
@@ -3,6 +3,7 @@ var app = app || {};
 app.AppView = Backbone.View.extend({
   el: '#main',
   render: function () {
+
     // this.$el.html( $('#appView').html() );
   }
 });
