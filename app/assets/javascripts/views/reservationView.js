cf1b486af5fb42dbb76a1badac182ef71e88f2a3
Libby Coy <libbycoy23@gmail.com>
2016-10-26 23:25:56 +1100

    a bunch of resview code that doesn't workand is commented out

--- app/assets/javascripts/views/reservationView.js
+++ app/assets/javascripts/views/reservationView.js
@@ -7,8 +7,24 @@ app.ReservationView = Backbone.View.extend({
     var resMaker = _.template(reservationTemplate);
     this.$el.html( resMaker(this.model.toJSON()) );
 
-    var $seats = this.$el.find('#seats');
-    ($seats).append('<img src="http://www.4wd.com/aux_incl/images.ashx?i=TR6701MB.jpg&partNo=CBU&w=50&h=50" alt="">');
+    // var $seats = this.$el.find('#seats');
+    //
+    // var p = app.burningFlights;
+    // var thisRow = p.attributes.airplane.row;
+    // var thisCol = p.attributes.airplane.column;
+    //
+    // seatCount = thisRow.to_i * thisCol.to_i;
+    //
+    // // adds in seats according to number of rows and columns multiplied together and appends them as seat image divs.
+    // // broken
+    // $(document).ready(function(){
+    //   for (var i = 0; i < seatCount; i++) {
+    //   ($seats).append('<img src="http://www.4wd.com/aux_incl/images.ashx?i=TR6701MB.jpg&partNo=CBU&w=50&h=50" alt="">');
+    // });
+
+
+
+
     // get the plane info from flight, find row column from plane, return the plane's seat fields
 
     // this.$el.html( $('#flightid-template').html() );
