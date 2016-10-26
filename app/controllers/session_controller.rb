6d7107a02c7464898ad0c7e5742e37edf04d2a7a
Pinky <dir.michaellin@gmail.com>
2016-10-26 20:49:59 +1100

    Add session controller and private methods inside application controller

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,24 @@
+class SessionController < ApplicationController
+
+  def new
+  end
+
+  def create
+
+    session[:current_user_id] = @user_id
+
+    user = User.find_by :email => params[:email]
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      session[:message] = "Invalid email or password"
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+end
