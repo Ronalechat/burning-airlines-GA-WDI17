c27df7269de6ed20a4515f45f690ac443cefebec
Pinky <dir.michaellin@gmail.com>
2016-10-26 16:32:47 +1100

    it's not working

--- app/assets/javascripts/views/reservationView.js
+++ app/assets/javascripts/views/reservationView.js
@@ -6,7 +6,7 @@ app.ReservationView = Backbone.View.extend({
   render: function () {
     var reservationTemplate = $('#flightid-template').html();
     var resMaker = _.template(reservationTemplate);
-    this.$el.html( resMaker(this.model.toJSON()) );
+    this.$el.html( resMaker(this.model.toJSON() ) );
     // this.$el.html( $('#flightid-template').html() );
   }
 })
